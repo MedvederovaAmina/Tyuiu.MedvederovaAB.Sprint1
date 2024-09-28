3a687386012b9e0b7b5e0c0f0b86ab52612ad668
Амина Медведерова <medvederovaamina@outlook.com>
2024-09-28 23:44:21 +0500

    Task2

---
+++ Tyuiu.MedvederovaAB.Sprint1.Task2.V13/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.MedvederovaAB.Sprint1.Task2.V13.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint1.Task2.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Медведерова А. Б. | ПКT6-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКT6-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет указанные расчеты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите расстояние в километрах:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Расстояние в милях = " + ds.ConvertMilesToKm(x));
+            Console.ReadLine();
+        }
+    }
+}
