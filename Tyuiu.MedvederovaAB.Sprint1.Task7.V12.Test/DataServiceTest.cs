97b38d266fc64fc93a17e2ab0c82c86ae4ac7f34
Амина Медведерова <medvederovaamina@outlook.com>
2024-10-02 21:46:14 +0500

    Task7

---
+++ Tyuiu.MedvederovaAB.Sprint1.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.MedvederovaAB.Sprint1.Task7.V12.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint1.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = 333;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.MedvederovaAB.Sprint1.Task7.V12/Program.cs
+++ Tyuiu.MedvederovaAB.Sprint1.Task7.V12/Program.cs
@@ -1,6 +1,6 @@
-﻿using Tyuiu.MedvederovaAB.Sprint1.Task6.V5.Lib;
+﻿using Tyuiu.MedvederovaAB.Sprint1.Task7.V12.Lib;
 
-namespace Tyuiu.MedvederovaAB.Sprint1.Task6.V5
+namespace Tyuiu.MedvederovaAB.Sprint1.Task7.V12
 {
     internal class Program
     {
@@ -11,24 +11,26 @@ namespace Tyuiu.MedvederovaAB.Sprint1.Task6.V5
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #6                                                              *");
-            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
             Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКTб-24-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* которая печатает симметричные слова  на экране.                         *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Введите строку:");
-            string str = Console.ReadLine();
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine(ds.CheckSymmetricalWords(str));
-            Console.ReadKey();
+            Console.WriteLine(ds.Calculate(x, y));
         }
     }
 }
