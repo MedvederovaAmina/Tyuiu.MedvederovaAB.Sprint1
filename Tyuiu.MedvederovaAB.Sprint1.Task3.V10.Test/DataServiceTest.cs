61e6b6b12fd768d3b97751bc123809931d6ea6ac
Амина Медведерова <medvederovaamina@outlook.com>
2024-10-16 12:17:28 +0500

    Task3

--- Tyuiu.MedvederovaAB.Sprint1.Task3.V10.Test/DataServiceTest.cs
+++ Tyuiu.MedvederovaAB.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ public class DataServiceTest
     public void ValidExpression()
     {
         DataService ds = new DataService();
-        double nomber = 23.6;
+        double nomber =  23.6;
         var res = ds.NumberToMoney(23.6);
         Assert.AreEqual($"23.6  руб. - это 23 руб. 60 коп.", res);
     }
